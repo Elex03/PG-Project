3e0075789f779177084f36028c86f3ad5841c202
Elex03 <eacuna042@gmail.com>
2024-04-15 23:17:59 -0600

    shaders v0

--- OpenGlIntroduction/src/Client/index.cpp
+++ OpenGlIntroduction/src/Client/index.cpp
@@ -1,17 +1,124 @@
-#include "window.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+const char* vertexShaderSource = R"glsl(
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+
+    void main() {
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+)glsl";
+
+const char* fragmentShaderSource = R"glsl(
+    #version 330 core
+    out vec4 FragColor;
+
+    void main() {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    }
+)glsl";
 
 int main() {
-    // Inicializar ventana
-    Window window(800, 600, "Mi Ventana GLFW");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Bucle principal
-    while (!glfwWindowShouldClose(window.getWindow())) {
-        // Procesar eventos
-        glfwPollEvents();
-
-        // Renderizar
-        // Aquí pondrías tu código de renderizado
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
     }
 
+    glfwMakeContextCurrent(window);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "Vertex shader compilation failed\n" << infoLog << std::endl;
+    }
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "Fragment shader compilation failed\n" << infoLog << std::endl;
+    }
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "Shader program linking failed\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLfloat vertices[] = {
+         0.5f,  0.5f, 0.0f,  // Top Right
+         0.5f, -0.5f, 0.0f,  // Bottom Right
+        -0.5f, -0.5f, 0.0f,  // Bottom Left
+    };
+
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glBindVertexArray(0);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+
+        glfwSwapBuffers(window);
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
+    glfwTerminate();
     return 0;
 }
--- OpenGlIntroduction/src/Engine/Window.cpp
+++ OpenGlIntroduction/src/Engine/Window.cpp
@@ -1,22 +1,28 @@
 #include "Window.h"
 
 Window::Window(int width, int height, const char* title) {
-    // Inicializa GLFW
+    
     glfwInit();
 
-    // Configura la ventana
     m_window = glfwCreateWindow(width, height, title, NULL, NULL);
     if (!m_window) {
         glfwTerminate();
         throw "Error al crear la ventana GLFW";
     }
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
 
-    // Hacer que la ventana sea el contexto principal
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+
+    const char* renderer = reinterpret_cast<const char*>(glGetString(GL_RENDERER));
+    std::cout << "Tarjeta gráfica: " << renderer << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
     glfwMakeContextCurrent(m_window);
 }
 
 Window::~Window() {
-    // Termina GLFW
     glfwDestroyWindow(m_window);
     glfwTerminate();
 }
