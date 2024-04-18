550d7094b46683aab0faa40ad2333eee329518c6
Elex03 <eacuna042@gmail.com>
2024-04-17 22:08:47 -0600

    Window Engine

--- OpenGlIntroduction/src/Engine/Window.cpp
+++ OpenGlIntroduction/src/Engine/Window.cpp
@@ -1,28 +1,19 @@
-#include "Window.h"
+#include "window.h"
 
 Window::Window(int width, int height, const char* title) {
-    
     glfwInit();
-
+    
     m_window = glfwCreateWindow(width, height, title, NULL, NULL);
     if (!m_window) {
         glfwTerminate();
         throw "Error al crear la ventana GLFW";
     }
-    if (glewInit() != GLEW_OK)
-        std::cout << "Error!" << std::endl;
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
-
-
-    const char* renderer = reinterpret_cast<const char*>(glGetString(GL_RENDERER));
-    std::cout << "Tarjeta gráfica: " << renderer << std::endl;
-
-    std::cout << glGetString(GL_VERSION) << std::endl;
     glfwMakeContextCurrent(m_window);
 }
 
 Window::~Window() {
+    // Termina GLFW
     glfwDestroyWindow(m_window);
     glfwTerminate();
 }
