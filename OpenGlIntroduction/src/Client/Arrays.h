ca587600d1f9a1df4035588d24e600990d74ae7c
Elex03 <eacuna042@gmail.com>
2024-04-19 21:10:14 -0600

    Arrays

---
+++ OpenGlIntroduction/src/Client/Arrays.h
@@ -0,0 +1,8 @@
+#pragma once
+float positions[] = {
+		 0.0f, 0.9f,
+		 0.9f, 0.0f,
+		 0.0f,-0.9f,
+		 -0.9f, 0.0f,
+		 0.0f, 0.9f,
+};
--- OpenGlIntroduction/src/Client/index.cpp
+++ OpenGlIntroduction/src/Client/index.cpp
@@ -1,5 +1,6 @@
 #include <Window.h>
 #include <Shader.h>
+#include <Arrays.h>
 
 
 void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
@@ -16,26 +17,15 @@ int main(void)
 {
 	Window window(800, 600, "Mi Ventana GLFW");
 
-	float positions[] = {
-		 0.0f, 0.9f,
-		 0.9f, 0.0f,
-		 0.0f,-0.9f,
-		 -0.9f, 0.0f,
-		 0.0f, 0.9f,
-	};
-
-
+	
 	int Longitud_Array = sizeof(positions) / sizeof(*positions);
 	unsigned int buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, Longitud_Array * sizeof(float), positions, GL_STATIC_DRAW);
 
-
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-
-
 
 	Shader Shader("0.494,0.518,0.969, 0");
 	glUseProgram(Shader.getprogram());
