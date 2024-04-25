3c4b90bd490d78c4f1ced123795223fd790282c8
Elex03 <eacuna042@gmail.com>
2024-04-24 22:34:29 -0600

    add estado

--- OpenGlIntroduction/src/Client/index.cpp
+++ OpenGlIntroduction/src/Client/index.cpp
@@ -25,25 +25,26 @@ int main(void)
 	glfwSetCursorPosCallback(window.getWindow(), cursor_position_callback);
 	glGenBuffers((Longitud_Array / 2), buffers.data());
 
-
+	bool estado = true;
 	while (!glfwWindowShouldClose(window.getWindow())) {
 
 		glClear(GL_COLOR_BUFFER_BIT);
-		glClearColor(0.145, 0.157, 0.314, 1);
-
-
-		for (int i = 1, j = 0; i < Longitud_Array && !glfwWindowShouldClose(window.getWindow()); i += 2, j++)
+		while (estado)
 		{
-			Shader Shader(Colors[j]);
-			glUseProgram(Shader.getprogram());
-			glBindBuffer(GL_ARRAY_BUFFER, buffers[j]);
-			glBufferData(GL_ARRAY_BUFFER, positions[(i - 1)][0] * sizeof(float), positions[i], GL_STATIC_DRAW);
-			glEnableVertexAttribArray(0);
-			glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-			glDrawArrays(GL_TRIANGLE_STRIP, 0, (positions[(i - 1)][0]) / 2);
+			glClearColor(0.145, 0.157, 0.314, 1);
+			for (int i = 1, j = 0; i < Longitud_Array && !glfwWindowShouldClose(window.getWindow()); i += 2, j++)
+			{
+				Shader Shader(Colors[j]);
+				glUseProgram(Shader.getprogram());
+				glBindBuffer(GL_ARRAY_BUFFER, buffers[j]);
+				glBufferData(GL_ARRAY_BUFFER, positions[(i - 1)][0] * sizeof(float), positions[i], GL_STATIC_DRAW);
+				glEnableVertexAttribArray(0);
+				glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+				glDrawArrays(GL_TRIANGLE_STRIP, 0, (positions[(i - 1)][0]) / 2);
+			}
+			glfwSwapBuffers(window.getWindow());
+			estado = false;
 		}
-
-		glfwSwapBuffers(window.getWindow());
 		glfwPollEvents();
 	}
 
