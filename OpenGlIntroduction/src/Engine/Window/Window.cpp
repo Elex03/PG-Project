362e73a211bf35413c579df92abd462932418fe4
Elex03 <eacuna042@gmail.com>
2024-05-16 21:23:39 -0600

    lab 4

--- OpenGlIntroduction/src/Engine/Window/Window.cpp
+++ OpenGlIntroduction/src/Engine/Window/Window.cpp
@@ -1,22 +1,34 @@
 #include "window.h"
-
 Window::Window(int width, int height, const char* title) {
-    glfwInit();
-    
-    m_window = glfwCreateWindow(width, height, title, NULL, NULL);
-    if (!m_window) {
-        glfwTerminate();
-        throw "Error al crear la ventana GLFW";
-    }
+	
+	glfwInit();
 
-    glfwMakeContextCurrent(m_window);
+	//Le indicamos a GLFW con que version de OpenGL queremos trabajar. En este caso estamos usando OpenGL 3.7
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
+	//Le indicamos GLFW que estamos usando solo funciones modernas
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    std::cout << glGetString(GL_VERSION) << std::endl;
+	//Establecemos el nombre que deseamos aparezca en pantalla
+	m_window = glfwCreateWindow(800, 800, "Primitivas Graficas", NULL, NULL);
+
+	//En caso que la ventana no se pueda crear entonces enviamos un error.
+	if (m_window == NULL) {
+
+		std::cout << "Fallo al tratar de crear la ventana usando GLFW" << std::endl;
+		glfwTerminate();
+	}
+
+	//Ponemos la ventana en el contexto actual de lo que queremos presentar en pantalla
+	glfwMakeContextCurrent(m_window);
 
 }
 Window::~Window() {
-    // Termina GLFW
-    glfwDestroyWindow(m_window);
-    glfwTerminate();
+	// Termina GLFW
+	glfwDestroyWindow(m_window);
+	glfwTerminate();
 }
+
+
+
--- OpenGlIntroduction/src/Engine/Window/Window.h
+++ OpenGlIntroduction/src/Engine/Window/Window.h
@@ -5,11 +5,10 @@
 
 class Window {
 public:
-    Window(int width, int height, const char* title);
+    Window( int width, int height, const char* title);
     ~Window();
 
     GLFWwindow* getWindow() const { return m_window; };
-
 
 private:
     GLFWwindow* m_window;
