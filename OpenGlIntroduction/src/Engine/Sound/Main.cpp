3c1b3587edae355e1f88e0d47a854d1161f8559c
Elex03 <eacuna042@gmail.com>
2024-05-26 20:01:33 -0600

    fix .dl;

--- OpenGlIntroduction/src/Engine/Sound/Main.cpp
+++ OpenGlIntroduction/src/Engine/Sound/Main.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
 #include<irrKlang/irrKlang.h>
 
-
-int main()
+using namespace irrklang;
+#pragma comment(lib, "irrKlang.lib") // link with irrKlang.dll
+int main(int argc, const char** argv)
 {
-	std::cout << "halley" << std::endl;
+    // start the sound engine with default parameters
+    ISoundEngine* engine = createIrrKlangDevice();
+
+    if (!engine)
+        return 0; // error starting up the engine
+    // play some sound stream, looped
+
+    engine->play2D("../../media/getout.ogg", true);
+    std::cout << "\nHello World!\n";
+
+    char i = 0;
+
+    while (i != 'q')
+    {
+        std::cout << "Press any key to play some sound, press 'q' to quit.\n";
+
+        // play a single sound
+        engine->play2D("../../media/bell.wav");
+
+        std::cin >> i; // wait for user to press some key
+    }
 }
