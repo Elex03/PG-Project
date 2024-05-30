2b3859b68eeede2585c76b746914675893445ff6
Elex03 <eacuna042@gmail.com>
2024-05-29 21:29:33 -0600

    add .model

---
+++ OpenGlIntroduction/src/Engine/models/Main.model.cpp
@@ -0,0 +1,79 @@
+//------- Ignore this ----------
+#include<filesystem>
+namespace fs = std::filesystem;
+//------------------------------
+
+#include"Model.h"
+#include <Window.h>
+
+const unsigned int width = 800;
+const unsigned int height = 800;
+
+
+int main()
+{
+    Window window(600, 800, "Pantalla");
+    // Load GLAD so it configures OpenGL
+    gladLoadGL();
+    // Specify the viewport of OpenGL in the Window
+    // In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+    glViewport(0, 0, width, height);
+
+    // Generates Shader object using shaders default.vert and default.frag
+    Shader shaderProgram("src/Engine/Models/default.vert", "src/Engine/Models/default.frag");
+
+    // Take care of all the light related things
+    glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+    glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+    glm::mat4 lightModel = glm::mat4(1.0f);
+    lightModel = glm::translate(lightModel, lightPos);
+
+    shaderProgram.Activate();
+    glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+    glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+
+    // Enables the Depth Buffer
+    glEnable(GL_DEPTH_TEST);
+
+    // Creates camera object
+    Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
+    std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
+    std::string modelPath = "/OpenGlIntroduction/src/Engine/resources/Models/grindstone/scene.gltf";
+
+    // Load in a model
+    Model model((parentDir + modelPath).c_str());
+
+    // Original code from the tutorial
+    // Model model("models/bunny/scene.gltf");
+
+    // Main while loop
+    while (!glfwWindowShouldClose(window.getWindow()))
+    {
+        // Specify the color of the background
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        // Clean the back buffer and depth buffer
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Handles camera inputs
+        camera.Inputs(window.getWindow());
+        // Updates and exports the camera matrix to the Vertex Shader
+        camera.updateMatrix(45.0f, 0.1f, 600.0f);
+
+        // Draw a model
+        model.Draw(shaderProgram, camera);
+
+        // Swap the back buffer with the front buffer
+        glfwSwapBuffers(window.getWindow());
+        // Take care of all GLFW events
+        glfwPollEvents();
+    }
+
+    // Delete all the objects we've created
+    shaderProgram.Delete();
+    // Delete window before ending the program
+    glfwDestroyWindow(window.getWindow());
+    // Terminate GLFW before ending the program
+    glfwTerminate();
+    return 0;
+}
